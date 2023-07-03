fccdf24a195967cd3b4a3e294cf63ff0a57b172b
Reza Hermawan <rezahermawan10969@gmail.com>
2023-07-03 11:01:03 +0700

    BottomNavigasi

---
+++ lib/views/bottomNavigation.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:foodies/providers/LoginRegisProvider.dart';
+import 'package:foodies/utils/myColorApp.dart';
+import 'package:foodies/views/addMenu/screenAddMenu.dart';
+import 'package:foodies/views/home/ScreenHome.dart';
+import 'package:foodies/views/profile/screenProfile.dart';
+import 'package:provider/provider.dart';
+
+class BottomNavMain extends StatefulWidget {
+  const BottomNavMain({super.key});
+
+  @override
+  State<BottomNavMain> createState() => _BottomNavMainState();
+}
+
+class _BottomNavMainState extends State<BottomNavMain> {
+  int _selectedIndex = 0;
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text('Home'),
+    Text('Add Resep'),
+    Text('Profile'),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final provIdUser = Provider.of<UserLoginProvider>(context);
+
+    Widget body = _widgetOptions.elementAt(_selectedIndex);
+    switch (_selectedIndex) {
+      case 0:
+        body = ScreenHome();
+        break;
+      case 1:
+        body = ScreeenAddMenu();
+        break;
+      case 2:
+        body = ScreenProfile();
+        break;
+    }
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: body,
+        bottomNavigationBar: BottomNavigationBar(
+          elevation: 10,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add),
+              label: 'Add Resep',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Profile',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          selectedItemColor: ColorConstants
+              .primaryColor, // ubah warna ikon yang dipilih menjadi hitam
+          unselectedItemColor: Colors.black.withOpacity(
+              0.6), // ubah warna ikon yang tidak dipilih menjadi hitam transparan
+        ),
+      ),
+    );
+  }
+}
+
+enum MenuItem {
+  Profil,
+  Pengaturan,
+  Logout,
+}
